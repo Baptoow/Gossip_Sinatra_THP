24e23a7c740f07036c045b4fb749a86898ec7846
Baptoow <baptistebarbera@gmail.com>
2020-10-19 20:04:58 +0200

    push

---
+++ README.md
@@ -0,0 +1,7 @@
+Hello voici mon premier programe avec sinatra 
+
+il faut : faire un bundle install 
+
+ce programme est un programe de gossip en ligne tu peut laisser un gossip et il sera mis en ligne par ce programe.
+
+merci 
---
+++ lib/controller.rb
@@ -0,0 +1,21 @@
+require 'gossip'
+
+
+class ApplicationController < Sinatra::Base
+
+  get '/' do 
+    erb :index, locals: {gossips: Gossip.all}
+  end
+
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+
+  post '/gossips/new/' do 
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    redirect '/'
+  end
+  get ':id' do
+    "Voici le numero du potin que tu veut :#{params['id']}!"
+  end
+end
